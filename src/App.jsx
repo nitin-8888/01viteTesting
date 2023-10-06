0f30960fe6546573b3c470d0ebddceca28468c99
nitin-8888 <jajnitin7@gmail.com>
2023-10-06 12:27:42 +0530

    setcounter

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+// let counter=5;
+
+let [counter, setCounter]=useState(0)
+
+const addValue=()=>{
+  counter<20?counter=counter+1:""
+  setCounter(counter);
+} 
+
+const remValue=()=>{
+  counter>0?counter=counter-1:"0"  
+  setCounter(counter);
+}
+
+  return (
+    <>
+      <h1>Hello Counter</h1>
+      <h2>Counter value:{counter}</h2>
+      <button onClick={addValue}>Add value</button><br/>
+      <button onClick={remValue}>Remove value</button>
+    </>
+  )
+}
+
+export default App
